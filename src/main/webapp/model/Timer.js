9165874af339e231f3af3f3d13fd97324a60f8ba
damenso <144603831+damenso@users.noreply.github.com>
2025-08-24 20:55:37 +0200

    timer type en current loop to gevoegd.

--- src/main/webapp/model/Timer.js
+++ src/main/webapp/model/Timer.js
@@ -7,6 +7,8 @@ export default class Timer {
         this.resetTimerButton = document.querySelector('#reset-timer-button');
         this.resumeTimerButton = document.querySelector('#resume-timer-button');
         this.timerView = document.querySelector('#timer');
+        this.timerType = document.querySelector('#timer-type');
+        this.loops = document.querySelector('#loops');
 
         this.timerService = new TimerService();
 
@@ -16,21 +18,25 @@ export default class Timer {
         this.resumeTimerButton?.addEventListener("click", (event) => this.resumeTimer(event));
 
         this.renderTimer();
-
-
         this.interval = null;
-
-         
     }
 
     renderTimer(){
         this.focusSeconds = this.timerService.getFocus() * 60;
         this.breakSeconds = this.timerService.getBreak() * 60;
         this.loopsTotal = this.timerService.getLoops();
-
+        this.phase = 'focus';
+        this.loopsLeft = Math.max(1, this.loopsTotal);
         this.remaining = this.focusSeconds;
-
         this.updateTimer();
+    }
+
+    updateStatus(){
+        if (this.timerType) this.timerType.textContent = (this.phase === 'focus' ? 'Focus' : 'Break');
+        if (this.loops) {
+            const currentLoop = this.loopsTotal - this.loopsLeft + 1;
+            this.loops.textContent = `${currentLoop}/${this.loopsTotal}`;
+        }
     }
 
     updateTimer(){
@@ -39,20 +45,48 @@ export default class Timer {
         this.timerView.textContent = `${minutes.toString().padStart(2,"0")}:${seconds.toString().padStart(2,"0")}`
     }
 
+    runTimer(){
+        if (this.remaining > 0){
+            this.remaining--;
+            this.updateTimer();
+            if (this.remaining === 0) this.switchTypeTimer();
+        }
+    }
+
+    switchTypeTimer(){
+        if (this.phase === 'focus'){
+            if (this.loopsLeft <= 1) {
+                clearInterval(this.interval);
+                this.interval = null;
+                this.updateStatus();
+                alert("All focus rounds have finished. Good job!");
+                return;
+            }
+            alert("Focus time's up, it's breaktime!");
+            this.phase = "break";
+            this.remaining = this.breakSeconds;
+            this.updateTimer();
+            this.updateStatus();
+        } else {
+            this.loopsLeft--;
+            this.phase = "focus";
+            this.remaining = this.focusSeconds;
+            this.updateTimer();
+            this.updateStatus();
+            alert(`The break is over let's start round ${this.loopsTotal - this.loopsLeft + 1}/${this.loopsTotal}`);
+        }
+    }
+
     startTimer(){
         this.timerService.createTimer(this.focusSeconds/60, this.breakSeconds/60, this.loopsTotal).catch(()=>{});
         clearInterval(this.interval);
         this.remaining = this.focusSeconds;
-        this.interval = setInterval(() => {
-
-            if (this.remaining > 0){
-                this.remaining--;
-                this.updateTimer();
-            } else {
-                clearInterval(this.interval);
-                alert("Focus time's up");
-            }
-        }, 1000);
+        this.phase = "focus";
+        this.loopsLeft = Math.max(1, this.loopsTotal);
+        this.remaining = this.focusSeconds;
+        this.updateTimer();
+        this.updateStatus();
+        this.interval = setInterval(() => this.runTimer(), 1000);
 
     }
 
@@ -63,21 +97,14 @@ export default class Timer {
 
     stopTimer(){
         clearInterval(this.interval);
+        this.interval = null;
         this.renderTimer();
     }
 
     resumeTimer(){
         this.timerService.resumeTimer().catch(()=>{});
-        clearInterval(this.interval);
-        this.interval = setInterval(() => {
-            if (this.remaining > 0) {
-                this.remaining--;
-                this.updateTimer();
-            } else {
-                clearInterval(this.interval);
-                alert("Time's up");
-            }
-        }, 1000);
+        if (this.interval) return;
+        this.interval = setInterval(() => this.runTimer(), 1000);
     }
 
 
